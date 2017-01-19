a88fc400198bb4a53414a3a690b8c10e6115155d
masarakki <masaki182@gmail.com>
2017-01-20 03:24:23 +0900

    fix

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,17 +1,14 @@
 import $ from 'jquery';
-import url from 'url';
 import path from 'path';
 import sanitize from 'sanitize-filename';
 
 const username = sanitize($('.node-info .username').text()).trim();
 const title = sanitize($('.node-info .title').text()).trim();
+const filename = path.join('iwara', username, `${title}.mp4`);
+const url = $('#download-options li:first a').attr('href');
 
 $('#download-button').on('click', (e) => {
   e.stopPropagation();
   e.preventDefault();
-  const file = $('#download-options li:first a').attr('href');
-  chrome.runtime.sendMessage({
-    url: url.resolve(location.toString(), file),
-    filename: path.join('iwara', username, `${title}.mp4`)
-  });
+  chrome.runtime.sendMessage({url, filename});
 });
