ba8bb1d2caa66e825d9c577281f1e912d1f738d4
masarakki <masaki182@gmail.com>
2019-11-10 00:45:14 +0900

    add-datetime

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -4,7 +4,8 @@ import sanitize from 'sanitize-filename';
 
 const username = sanitize($('.node-info .username').text()).trim();
 const title = sanitize($('.node-info .title').text()).trim();
-const filename = path.join('iwara', username, `${title}.mp4`);
+const date = document.querySelector('.node-info .submitted').textContent.match(/作成日:(.+)/)[1].trim().replace(/ |-|:/g, '');
+const filename = path.join('iwara', username, `${date}-${title}.mp4`);
 
 $('#download-button').on('click', (e) => {
   e.stopPropagation();
